60f1454189d08fd3b2dd36377833ca944c813c78
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-01 07:44:41 -0500

    [TASK] Add Content and Total component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,6 @@
 import Header from './Header'
+import Content from './Content'
+import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -12,16 +14,10 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,11 @@
+const Content = ( {part, exercises}) => {
+  return (
+    <>
+       <p>
+        {part} {exercises}
+      </p>
+    </>
+  )
+}
+
+export default Content
