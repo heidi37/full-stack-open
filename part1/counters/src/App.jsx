ee2fe57d5a6f230649e8cc69d8b40b99f4f6764c
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-03 07:17:14 -0500

    [TASK] Implement sample counter application

---
+++ part1/counters/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import Display from "./Display"
+import Button from "./Button"
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter((prevState) => prevState + 1)
+  }
+  
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter((prevState) => prevState - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <>
+    <Display counter={counter} />
+    <Button text="+1" onclick={increaseByOne} />
+    <Button text="-1" onclick={decreaseByOne} />
+    <Button text="Reset" onclick={setToZero} />
+    </>
+  )
+}
+
+export default App
---
+++ part1/counters/src/Button.jsx
@@ -0,0 +1,3 @@
+const Button = ( {text, onclick} ) => <div><button onClick={onclick}>{text}</button></div>
+
+export default Button
