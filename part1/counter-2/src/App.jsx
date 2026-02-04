f891884334267fdaaed7a0cc31dd4f2c50a156c8
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-04 06:19:17 -0500

    [TASK] Refactor counter into componenets

--- part1/counter-2/src/App.jsx
+++ part1/counter-2/src/App.jsx
@@ -1,34 +1,42 @@
 import { useState } from "react"
+import History from "./History"
+import Button from "./Button"
 
 const App = () => {
   const [clicks, setClicks] = useState({
-    left: 0, right: 0
+    left: 0,
+    right: 0,
   })
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  function setClicksLeft() {
-      setClicks({
-        right: right,
-        left: left + 1
-      })
+
+  function handleLeftClick() {
+    setAll(allClicks.concat("L"))
+    setClicks({
+      ...clicks,
+      left: clicks.left + 1,
+    })
+    setTotal((prev) => prev + 1)
   }
 
-  function setClicksRight(prev) {
-      setClicks({
-        ...prev,
-        right: right + 1
-      })
+  function handleRightClick() {
+    setAll(allClicks.concat('R'))
+    setClicks({
+      ...clicks,
+      right: clicks.right + 1,
+    })
+    setTotal((prev) => prev + 1)
   }
 
   return (
     <div>
       {clicks.left}&nbsp;
-      <button onClick={() => setClicksLeft()}>
-        left
-      </button>
-      <button onClick={() => setClicksRight()}>
-        right
-      </button>
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
       &nbsp;{clicks.right}
+      <History allClicks={allClicks}/>
+      <p><strong>Total: {total}</strong></p>
     </div>
   )
 }
---
+++ part1/counter-2/src/History.jsx
@@ -0,0 +1,18 @@
+const History = ( {allClicks} ) => {
+
+
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+export default History
