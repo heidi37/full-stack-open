51467d661c710710019bfc3156d243a37e206174
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-05 07:10:36 -0500

    [TASK] Completes exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,10 +7,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [scores, setScores] = useState([])
 
-  const handleGoodClick = () => setGood(prev => prev + 1)
-  const handleNeutralClick = () => setNeutral(prev => prev + 1)
-  const handleBadClick = () => setBad(prev => prev + 1)
+  const total = good + neutral + bad
+  const average = scores.length > 0 ? scores.reduce((total, value) => total + value, 0) / scores.length : 0
+  const positive = scores.length > 0 ? good / total : 0
+
+  const handleGoodClick = () => {
+    setGood(prev => prev + 1)
+    addScoreValue(1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(prev => prev + 1)
+    addScoreValue(0)
+  }
+  const handleBadClick = () => {
+    setBad(prev => prev + 1)
+    addScoreValue(-1)
+  }
+
+  const addScoreValue = (scoreValue) => setScores(scores.concat(scoreValue))
 
   return (
     <div>
@@ -19,7 +35,11 @@ const App = () => {
       <button onClick={() => handleNeutralClick()}>neutral</button>
       <button onClick={() => handleBadClick()}>bad</button>
       <h2>statistics</h2>
-      <p>good: {good}<br/>neutral: {neutral}<br/>bad: {bad}</p>
+      <p>good: {good}<br/>neutral: {neutral}<br/>bad: {bad}<br/>
+      all: {total} <br/>
+      average: {average} <br/>
+      positive: {positive * 100}% <br/>
+      </p>
     </div>
   )
 }
