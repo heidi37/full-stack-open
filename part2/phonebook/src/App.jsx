b1bcb3063452cc4fad6d10cec0cfd046e28f31b5
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-08 14:07:10 -0500

    [FEATURE] Alert when a duplicate name has been entered

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [newName, setNewName] = useState("")
 
   const handleChange = (event) => {
     setNewName(event.target.value)
@@ -12,27 +10,33 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    setPersons(prev => {
-      return [ ...prev,
-        {name: newName}
-      ]
-    })
-    setNewName('')
+      if (persons.some(person => person.name === newName))
+      {
+        alert(`${newName} is already added to the phonebook.`)
+        return
+      }
+      setPersons((prev) => {
+          return [...prev, { name: newName }]
+      })
+      console.log(persons)
+      setNewName("")
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
-          <button onClick={addName} type="submit">add</button>
+          <button type="submit">
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => {
+      {persons.map((person) => {
         return <p key={person.name}>{person.name}</p>
       })}
     </div>
