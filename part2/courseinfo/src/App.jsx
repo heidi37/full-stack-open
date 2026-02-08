b1602746c379361a25c107d101818bf0689ac252
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-08 07:35:43 -0500

    [TASK] Completes exercise 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,12 +1,23 @@
-const Header = (props) => <h1>{props.course}</h1>
+const Header = (props) => <h1>{props.course.name}</h1>
 
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
+const Course = (props) => {
+  return (
+    <>
+    <Header course={props.course}/>
+    <Content course={props.course} />
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+      <div>
+        {props.course.parts.map((part) => {
+          return <Part part={part} />
+        })}
+    </div>
+  )
+}
 
 const Part = (props) => (
   <p>
@@ -14,40 +25,36 @@ const Part = (props) => (
   </p>
 )
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3
       },
-    ],
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      />
-    </div>
-  )
+    return <Course course={course} />
 }
 
 export default App
