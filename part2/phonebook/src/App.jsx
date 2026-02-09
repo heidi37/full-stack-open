70596000f144dddf5f2df68ef0174eefbb3f743d
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-09 08:12:26 -0500

    [TASK] Data fetched with axios and json server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [filter, setFilter] = useState("")
+
+  useEffect(() => {
+    console.log("effect")
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const handleChangeName = (event) => {
     setNewName(event.target.value)
@@ -27,26 +31,31 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-      if (persons.some(person => person.name === newName))
-      {
-        alert(`${newName} is already added to the phonebook.`)
-        return
-      }
-      setPersons((prev) => {
-          return [...prev, { name: newName, number:newNumber }]
-      })
-      setNewName("")
-      setNewNumber("")
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook.`)
+      return
+    }
+    setPersons((prev) => {
+      return [...prev, { name: newName, number: newNumber }]
+    })
+    setNewName("")
+    setNewNumber("")
   }
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <Filter onChange={handleChangeFilter} filter={filter}/>
+      <Filter onChange={handleChangeFilter} filter={filter} />
       <h2>Add a new</h2>
-      <PersonForm onSubmit={addName} newName={newName} onChangeName={handleChangeName} newNumber={newNumber} onChangeNumber={handleChangeNumber}/>
+      <PersonForm
+        onSubmit={addName}
+        newName={newName}
+        onChangeName={handleChangeName}
+        newNumber={newNumber}
+        onChangeNumber={handleChangeNumber}
+      />
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter}/>
+      <Persons persons={persons} filter={filter} />
     </div>
   )
 }
