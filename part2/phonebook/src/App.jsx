3815d63acbcd0bd7a19bc67d90ec3f837baef53a
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-14 06:20:45 -0500

    [TASK] Complete exercise 2.13 extract services to separate file

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react"
-import axios from "axios"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
+import personService from "./services/persons"
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,8 +11,8 @@ const App = () => {
   const [filter, setFilter] = useState("")
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data)
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons)
     })
   }, [])
 
@@ -40,11 +40,10 @@ const App = () => {
       number: newNumber,
     }
 
-    axios
-      .post("http://localhost:3001/persons", personObject)
-      .then((response) => {
+    personService.create(personObject)
+      .then((returnedPerson) => {
         setPersons((prev) => {
-          return [...prev, { ...personObject }]
+          return [...prev, { ...returnedPerson }]
         })
         setNewName("")
         setNewNumber("")
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  //return just the data no the whole http response
+  //We no longer return the promise returned by axios directly. Instead, we assign the promise to the request variable and call its then method
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { 
+  getAll, 
+  create, 
+  update 
+}
