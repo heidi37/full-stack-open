a282b9f94b7df21b998869fb75a97db43beae115
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-15 21:00:13 -0500

    [FEATURE] implement country search

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import countryService from './services/countries'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState(null)
+  
+  // useEffect(() => {
+  //   countryService.getAll().then((initialCountries) => {
+  //     console.log(initialCountries.filter(country => country.includes))
+  //   })
+  // }, [countries])
+  console.log(countries)
+
+  const handleChange = function(event){
+    countryService.getAll().then((allCountries) => {
+      const results = allCountries.filter(country => country.name.common.includes(event.target.value))
+      setCountries(results.map(result => result.name.common))
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Countries</h1>
+      <label htmlFor="findField">Find Countries:</label>
+      <input type="text" id="findField" onChange={handleChange}></input>
+      {countries && Object.keys(countries).length <= 10 && countries.map(country => <p key={countries.indexOf(country)}>{country}</p>)}
+      {countries && Object.keys(countries).length > 10 ? <p>Too many results. Be more specific.</p> : ""}
+      {/* {countries && Object.keys(countries).length === 1 ? <img src={countries[0].flags.png} alt={countries[0].flags.alt} /> : ""} */}
     </>
   )
 }
