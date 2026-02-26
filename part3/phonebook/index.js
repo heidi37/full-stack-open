720ed5294c1ed6e5a412190f93e578c1cf775e6d
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-02-26 07:24:47 -0500

    [TASK] Complete exercises 3.7-8 using correct morgan tokens

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,8 +6,11 @@ const app = express()
 var morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('combined'))
+morgan.token('body', function getId (req) {
+  return JSON.stringify(req.body)
+})
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
   response.json(db)
