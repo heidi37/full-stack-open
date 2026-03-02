f4459f3f31548c8cf2c2d8a2c89d31917d35c220
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-03-02 07:20:59 -0500

    [TASK] Set up separate front and back end

---
+++ part3/notes-frontend/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react"
+import noteService from "./services/notes"
+import Note from "./components/Note"
+import Notification from "./components/Notification"
+import Footer from './components/Footer'
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  //The App component now controls the behavior of the input element
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes)
+    })
+  }, [])
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote("")
+    })
+  }
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id === id ? returnedNote : note)))
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {errorMessage && <Notification message={errorMessage} />}
+      <div>
+        <button onClick={() => setShowAll((prev) => !prev)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          placeholder="a new note..."
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
