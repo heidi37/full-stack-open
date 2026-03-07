0803f5b7a0fff8621c084df1fb76fe9048b74991
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-03-07 06:17:24 -0500

    [TASK] Support render server port

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -107,7 +107,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 //Starts the server with app.listen()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
