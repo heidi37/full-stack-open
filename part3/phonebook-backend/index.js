eed3fd43427cd8d96a785da41913972fc05e144f
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-03-09 06:41:45 -0400

    [FEATURE] Custom number validation

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -59,12 +59,6 @@ app.get("/info", (request, response) => {
 app.post("/api/persons", (request, response, next) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    })
-  }
-
   if (persons.find((person) => person.name === body.name)) {
     return response.status(400).json({
       error: "name must be unique",
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -14,8 +14,19 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
+    required: [true, 'Name required']
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{7,8}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Number required']
+  }
 })
 
 personSchema.set('toJSON', {
