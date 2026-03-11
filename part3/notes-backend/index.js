3a6ee88c2387804f5d94b678b08973326a27013f
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-03-11 07:29:05 -0400

    [TASK] Move controllers to separate file

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,8 +1,8 @@
 //Imports
 const express = require('express')
-const Note = require('./models/note')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const notesRouter = require('./controllers/notes')
 
 //Creates the Express app - a function that is used to create an Express application stored in the app variable:
 const app = express()
@@ -19,79 +19,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
-
-//Defines routes
-app.get('/', (request, response) => {
-  //the request is answered by using the send method of the response object.
-  response.send('<h1>Hello People!!!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    //If the next function is called with an argument, then the execution will continue to the error handler middleware.
-    .catch(error => next(error))
-})
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body
-
-  Note.findById(request.params.id)
-    .then(note => {
-      if (!note) {
-        return response.status(404).end()
-      }
-
-      note.content = content
-      note.important = important
-
-      return note.save().then((updatedNote) => {
-        response.json(updatedNote)
-      })
-    })
-    .catch(error => next(error))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
+app.use('/api/notes', notesRouter)
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
