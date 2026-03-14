9bc83f84c2baa712e597accd1f71c01b466f1122
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-03-14 16:49:05 -0400

    [TASK] Bloglist running as npm app, ex 4.1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://hfryzell_db_user:nfHJY26f7szjTHMg@cluster0.uwclkmr.mongodb.net/blogList?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
