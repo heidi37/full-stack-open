b1b0645c482940b8df402aaf93735822bf14b750
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-03-14 18:04:42 -0400

    [TASK] Accept incoming change

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,15 +4,6 @@ require('dotenv').config()
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
 
